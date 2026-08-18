55e48226ee61867b3e54c1269fc6b5b1c98bd3e8
Nalu <pordosol699@gmail.com>
2026-08-18 16:10:49 -0300

    ajustando supavase

--- mobile-pontuaAi/src/config/supabase.js
+++ mobile-pontuaAi/src/config/supabase.js
@@ -1,6 +1,14 @@
 import { createClient } from '@supabase/supabase-js';
+import AsyncStorage from '@react-native-async-storage/async-storage';
 import { SUPABASE_URL, SUPABASE_KEY } from './constants';
 
-const supabaseClient = createClient(SUPABASE_URL, SUPABASE_KEY);
+const supabaseClient = createClient(SUPABASE_URL, SUPABASE_KEY, {
+  auth: {
+    storage: AsyncStorage,
+    autoRefreshToken: true,
+    persistSession: true,
+    detectSessionInUrl: false,
+  },
+});
 
 export default supabaseClient;
